348c8286d60933f8f999a81d5526eefa83e33e05
xjh <414581775@qq.com>
2020-11-13 02:08:59 -0500

    resnet encoder

---
+++ model/backbone.py
@@ -0,0 +1,80 @@
+
+import torch
+import torch.nn as nn
+from model.blocks import BasicBlock, BottleNeck
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block_type_index, block_num_index):
+        super().__init__()
+        self.block_type = [BasicBlock, BottleNeck]
+        self.block_num = [[2, 2, 2, 2],
+                          [3, 4, 6, 3],
+                          [3, 4, 6, 3],
+                          [3, 4, 23, 3],
+                          [3, 8, 36, 3]]
+        self._construct_net(self.block_type[block_type_index], self.block_num[block_num_index])
+
+    def _construct_net(self, block, num_block, num_classes=100):
+        self.in_channels = 64
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, padding=1, bias=False, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True))
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        # we use a different inputsize than the original paper
+        # so conv2_x's stride is 1
+        self.conv2_x = self._make_layer(block, 64, num_block[0], 2)
+        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+
+    def _make_layer(self, block, out_channels, num_blocks, stride):
+        """make resnet layers(by layer i didnt mean this 'layer' was the
+        same as a neuron netowork layer, ex. conv layer), one layer may
+        contain more than one residual block
+        Args:
+            block: block type, basic block or bottle neck block
+            out_channels: output depth channel number of this layer
+            num_blocks: how many blocks per layer
+            stride: the stride of the first block of this layer
+        Return:
+            return a resnet layer
+        """
+
+        # we have num_block blocks per layer, the first block
+        # could be 1 or 2, other blocks would always be 1
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, stride))
+            self.in_channels = out_channels * block.expansion
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        output_list=[]
+        output = self.conv1(x)
+        output = self.conv2_x(output)
+        output_list.append(output)
+        output = self.conv3_x(output)
+        output_list.append(output)
+        output = self.conv4_x(output)
+        output_list.append(output)
+        output = self.conv5_x(output)
+        output_list.append(output)
+        return output_list
+
+def resnet_wrapper(name):
+    if name == "resnet18":
+        return ResNet(0, 0)
+    elif name == "resnet34":
+        return ResNet(0, 1)
+    elif name == "resnet50":
+        return ResNet(1, 2)
+    elif name == "resnet101":
+        return ResNet(1, 3)
+    elif name == "resnet152":
+        return ResNet(1, 4)
--- model/models.py
+++ model/models.py
@@ -1,40 +1,31 @@
 import torch
 import torch.nn as nn
-
-
+from model.backbone import resnet_wrapper
 class encoder(nn.Module):
     def __init__(self, opt):
         super(encoder, self).__init__()
         self.opt = opt
-        self.pose_dim = 4
+
         # image encoder
         assert (opt.nfeature % 4 == 0)
-        self.feature_maps = (opt.nfeature // 4, opt.nfeature // 2, opt.nfeature)
-        self.image_encoder = nn.Sequential(
-            nn.Conv2d(3, self.feature_maps[0], 4, 2, 1),
-            nn.Dropout2d(p=opt.dropout, inplace=True),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.feature_maps[0], self.feature_maps[1], 4, 2, 1),
-            nn.Dropout2d(p=opt.dropout, inplace=True),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(self.feature_maps[1], self.feature_maps[2], 4, 2, 1),
-        )
+        self.image_encoder = resnet_wrapper("resnet50")
 
         # pose encoder
         self.pose_encoder = nn.Sequential(
-            nn.Linear(self.opt.npose, self.opt.nfeature),
+            nn.Linear(self.opt.npose, self.opt.nfeature_pose),
             nn.Dropout(p=opt.dropout, inplace=True),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(self.opt.nfeature, self.opt.nfeature),
+            nn.Linear(self.opt.nfeature_pose, self.opt.nfeature_pose),
             nn.Dropout(p=opt.dropout, inplace=True),
             nn.LeakyReLU(0.2, inplace=True),
-            nn.Linear(self.opt.nfeature, self.opt.nfeature)
+            nn.Linear(self.opt.nfeature_pose, self.opt.image_size//32)
         )
 
     def forward(self, images, poses):
-        bsize = images.size(0)
-        h = self.image_encoder(images.view(bsize, 3, self.opt.height, self.opt.width))
-        h += self.pose_encoder(poses.view(bsize, self.pose_dim))
+        images = images.view(-1, images.size(1)//self.opt.camera_num, self.opt.image_size, self.opt.image_size)
+        poses = poses.view(-1, images.size(1)//self.opt.camera_num)
+        h = self.image_encoder(images)[-1]
+        h += self.pose_encoder(poses)
         return h
 
 
@@ -43,7 +34,7 @@ class decoder(nn.Module):
         super(decoder, self).__init__()
         self.opt = opt
 
-        self.feature_maps = (self.opt.nfeature // 4, self.opt.nfeature // 2, self.opt.nfeature)
+        self.feature_maps = (self.opt.nfeature_image // 4, self.opt.nfeature_image // 2, self.opt.nfeature_image)
 
         self.image_decoder = nn.Sequential(
             nn.ConvTranspose2d(self.feature_maps[2], self.feature_maps[1], 4, 2, 1),
