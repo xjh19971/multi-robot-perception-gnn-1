f0c8e873c7761cd51602100e681e919c3a100069
xjh <414581775@qq.com>
2020-12-13 01:05:16 -0500

    training + dataloader for flightmare

--- dgl/training.py
+++ dgl/training.py
@@ -126,7 +126,7 @@ def train_dgl(model, dataloader, optimizer, epoch, stats, opt, log_interval=50):
         pred_depth = model(data)
         depths = data.ndata['depth']        
         depths  = depths.view((-1, opt.camera_num, 1, opt.image_size, opt.image_size))
-        loss = compute_smooth_L1loss(depths, pred_depth, opt.dataset)
+        loss = compute_smooth_L1loss(depths, pred_depth, dataset=opt.dataset)
         train_loss += loss
         if not math.isnan(loss.item()):
             loss.backward()
@@ -151,7 +151,7 @@ def test_dgl(model, dataloader, stats, opt):
             pred_depth = model(data)
             depths = data.ndata['depth']
             depths  = depths.view((-1, opt.camera_num, 1, opt.image_size, opt.image_size))
-            test_loss += compute_smooth_L1loss(depths, pred_depth, opt.dataset)
+            test_loss += compute_smooth_L1loss(depths, pred_depth, dataset=opt.dataset)
             batch_num += 1
     avg_test_loss = test_loss / batch_num
     return [avg_test_loss]
