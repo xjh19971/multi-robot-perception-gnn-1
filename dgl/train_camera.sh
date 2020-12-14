e04a7f5ab9a8f6ad6b0fdd4936e30dabc2bb6b47
xjh <414581775@qq.com>
2020-12-14 00:19:27 -0500

    noise dataloader

--- dgl/train_camera.sh
+++ dgl/train_camera.sh
@@ -2,24 +2,6 @@
 # Allows named arguments
 set -k
 for camera_idx in 01234; do
-    for lrt in 0.005; do
-	for batch_size in 8; do
-		for seed in 1; do
-		    dataset="airsim"
-                    model_dir="airsim_models"
-		    model="model=single_view-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-seed=1"
-	       	    python -u training.py \
-		    -seed $seed \
-		    -camera_idx $camera_idx \
- 		    -dataset $dataset \
-		    -model_dir $model_dir \
-		    -model_file $model \
-		    -pretrained \
-	    done
-	    done
-    done
-    done
-for camera_idx in 01234; do
 	for lrt in 0.005; do
 		for batch_size in 8; do
 			for seed in 1; do
@@ -33,8 +15,27 @@ for camera_idx in 01234; do
 					-model_dir $model_dir \
 					-model_file $model \
 					-pretrained \
-				        -apply_noise_idx 1 	
+				  -apply_noise_idx 0
 			done
 		done
 	done
 done
+for camera_idx in 01234; do
+    for lrt in 0.005; do
+	    for batch_size in 8; do
+		      for seed in 1; do
+		    dataset="airsim"
+        model_dir="airsim_models"
+		    model="model=single_view-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-seed=1"
+	       	    python -u training.py \
+		    -seed $seed \
+		    -camera_idx $camera_idx \
+ 		    -dataset $dataset \
+		    -model_dir $model_dir \
+		    -model_file $model \
+		    -pretrained
+	    done
+	    done
+    done
+    done
+
