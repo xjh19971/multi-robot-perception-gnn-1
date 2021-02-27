834a7d480f191db7558376b603cb309b2bd47105
xjh <414581775@qq.com>
2021-02-27 01:40:58 -0500

    train

--- dgl/train_camera_warehouse_gcn_seg.sh
+++ dgl/train_camera_warehouse_gcn_seg.sh
@@ -2,14 +2,14 @@
 # Allows named arguments
 set -k
 gpu_idx=0
-python -u training_seg.py -seed 1 -camera_idx 01234 -dataset warehouse-dgl -model_dir warehouse_models -model_file "model=multi_view_gcn-bsize=8-lrt=0.005-camera_idx=01234-backbone=mobilenetv2-seed=1-apply_noise_idx=None" -model multi_view_dgl -pretrained -backbone mobilenetv2 -task seg -gpu_idx $gpu_idx
+python -u training_seg.py -seed 1 -camera_idx 01234 -dataset warehouse-dgl -model_dir warehouse_seg_models -model_file "model=multi_view_gcn-bsize=8-lrt=0.005-camera_idx=01234-backbone=mobilenetv2-seed=1-apply_noise_idx=None" -model multi_view_dgl -pretrained -backbone mobilenetv2 -task seg -gpu_idx $gpu_idx
 for camera_idx in 01234; do
 	for lrt in 0.005; do
 		for batch_size in 8; do
 			for seed in 1; do
 				for apply_noise_idx in 0 01 012; do
 					dataset="warehouse-noise-dgl"
-					model_dir="warehouse_models"
+					model_dir="warehouse_seg_models"
 					model="model=multi_view_gcn-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-backbone=mobilenetv2-seed=1-apply_noise_idx="$apply_noise_idx""
 					python -u training_seg.py \
 						-seed $seed \
