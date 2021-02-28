c43d13fbdccb12d47d6f6c05afd56e37f09d13ff
xjh <414581775@qq.com>
2021-02-28 02:44:49 -0500

    miou

--- dgl/eval_seg.py
+++ dgl/eval_seg.py
@@ -151,7 +151,7 @@ def compute_seg_metric(gt, pred, output_dim, activation=None):
     if activation == 'softmax':
         pred = torch.nn.Softmax(dim=1)(pred)
     pr = torch.argmax(pred, dim=1)
-    pr = one_hot(pr, output_dim).transpose(0,3,1,2)
+    pr = one_hot(pr, output_dim).transpose(2,3).transpose(1,2)
     meaniou = compute_meaniou(pr, gt)
     return meaniou
 
