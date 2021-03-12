95e54ab973b5e83adf8bcae5f6cb23a511f6e405
xjh19971 <jx1190@nyu.edu>
2021-03-12 01:52:13 -0500

    scripts

--- dgl/eval_camera_airsim_gcn2_seg.sh
+++ dgl/eval_camera_airsim_gcn2_seg.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # Allows named arguments
 set -k
-python -u eval_seg.py -seed 1 -camera_idx 01234 -dataset airsim -model_dir airsim_seg_models -model_file "best-model=single_view-bsize=8-lrt=0.005-camera_idx=01234-backbone=mobilenetv2-seed=1-apply_noise_idx=None"
+python -u eval_seg.py -seed 1 -camera_idx 01234 -dataset airsim-dgl -model_dir airsim_seg_models -model_file "best-model=multi_view_gcn_2-bsize=8-lrt=0.005-camera_idx=01234-backbone=mobilenetv2-seed=1-apply_noise_idx=None"
 for camera_idx in 01234; do
 	for lrt in 0.005; do
 		for batch_size in 8; do
 			for seed in 1; do
 				for apply_noise_idx in 0; do
-					dataset="airsim-noise"
+					dataset="airsim-noise-dgl"
 					model_dir="airsim_seg_models"
-					model="best-model=single_view-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-backbone=mobilenetv2-seed=1-apply_noise_idx="$apply_noise_idx""
+					model="best-model=multi_view_gcn_2-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-backbone=mobilenetv2-seed=1-apply_noise_idx="$apply_noise_idx""
 					python -u eval_seg.py \
 						-seed $seed \
 						-camera_idx $camera_idx \
--- dgl/train_camera_airsim_seg.sh
+++ dgl/train_camera_airsim_seg.sh
@@ -7,7 +7,7 @@ for camera_idx in 01234; do
 	for lrt in 0.005; do
 		for batch_size in 8; do
 			for seed in 1; do
-				for apply_noise_idx in 0 01 012; do
+				for apply_noise_idx in 0; do
 					dataset="airsim-noise"
 					model_dir="airsim_seg_models"
 					model="model=single_view-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-backbone=mobilenetv2-seed=1-apply_noise_idx="$apply_noise_idx""
