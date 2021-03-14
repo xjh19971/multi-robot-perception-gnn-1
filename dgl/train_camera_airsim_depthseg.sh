d0af065388521d325398d6aaed46e50aeac36236
xjh19971 <jx1190@nyu.edu>
2021-03-14 01:46:38 -0500

    depthseg

---
+++ dgl/train_camera_airsim_depthseg.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Allows named arguments
+set -k
+gpu_idx=0
+for camera_idx in 01234; do
+	for lrt in 0.005; do
+		for batch_size in 8; do
+			for seed in 1; do
+				for apply_noise_idx in 0; do
+					dataset="airsim-noise"
+					model_dir="airsim_depthseg_models"
+					model="model=single_view-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-backbone=mobilenetv2-seed=1-apply_noise_idx="$apply_noise_idx""
+					python -u training.py \
+						-seed $seed \
+						-camera_idx $camera_idx \
+						-dataset $dataset \
+						-model_dir $model_dir \
+						-model_file $model \
+						-pretrained \
+						-apply_noise_idx $apply_noise_idx\
+						-backbone "mobilenetv2" \
+						-task depthseg \
+						-gpu_idx $gpu_idx
+				done
+			done
+		done
+	done
+done
+python -u training.py -seed 1 -camera_idx 01234 -dataset airsim -model_dir airsim_depthseg_models -model_file "model=single_view-bsize=8-lrt=0.005-camera_idx=01234-backbone=mobilenetv2-seed=1-apply_noise_idx=None" -pretrained -backbone mobilenetv2 -gpu_idx $gpu_idx -task depthseg
+
---
+++ dgl/train_camera_airsim_gcn2_depthseg.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Allows named arguments
+set -k
+gpu_idx=0
+python -u training.py -seed 1 -camera_idx 01234 -dataset airsim-dgl -model_dir airsim_depthseg_models -model_file "model=multi_view_gcn_2-bsize=8-lrt=0.005-camera_idx=01234-backbone=mobilenetv2-seed=1-apply_noise_idx=None" -model multi_view_dgl -pretrained -backbone mobilenetv2 -gpu_idx $gpu_idx -multi_gcn -compress_gcn -task depthseg
+for camera_idx in 01234; do
+	for lrt in 0.005; do
+		for batch_size in 8; do
+			for seed in 1; do
+				for apply_noise_idx in 0; do
+					dataset="airsim-noise-dgl"
+					model_dir="airsim_depthseg_models"
+					model="model=multi_view_gcn_2-bsize="$batch_size"-lrt="$lrt"-camera_idx="$camera_idx"-backbone=mobilenetv2-seed=1-apply_noise_idx="$apply_noise_idx""
+					python -u training.py \
+						-seed $seed \
+						-camera_idx $camera_idx \
+						-dataset $dataset \
+						-model_dir $model_dir \
+						-model_file $model \
+						-model "multi_view_dgl"\
+						-pretrained \
+						-apply_noise_idx $apply_noise_idx\
+						-backbone "mobilenetv2" \
+						-gpu_idx $gpu_idx \
+						-task depthseg \
+						-multi_gcn \
+						-compress_gcn
+				done
+			done
+		done
+	done
+done
+
