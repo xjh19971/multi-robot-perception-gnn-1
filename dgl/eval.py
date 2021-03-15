c766d3347bdd3177fadc57beb3cc380dc932845f
xjh19971 <jx1190@nyu.edu>
2021-03-15 02:19:14 -0400

    eval

--- dgl/eval.py
+++ dgl/eval.py
@@ -130,9 +130,9 @@ def compute_depth_metric(gt, pred, dataset='airsim-mrmps-data'):
 def compute_seg_metric(gt, pred, output_dim, activation=None):
     gt = one_hot(gt, output_dim).squeeze(2).permute(0,1,4,2,3)
     gt = gt.view(-1, output_dim, opt.image_size, opt.image_size)
+    pred = pred.view(-1, output_dim, opt.image_size, opt.image_size)
     if activation == 'softmax':
         pred = torch.nn.Softmax(dim=1)(pred)
-    pred = pred.view(-1, output_dim, opt.image_size, opt.image_size)
     pr = torch.argmax(pred, dim=1).squeeze(2)
     pr = one_hot(pr, output_dim).permute(0,3,1,2)
     meaniou = compute_meaniou(pr, gt)
